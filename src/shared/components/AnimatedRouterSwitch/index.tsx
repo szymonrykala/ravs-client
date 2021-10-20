b9b00e9255d17bfa0885a14c51c2fc8f519e6f34
Szymon Rykała <szymon.rykala@atos.net>
2021-10-20 15:15:16 +0200

    add animated router switch

---
+++ src/shared/components/AnimatedRouterSwitch/index.tsx
@@ -0,0 +1,31 @@
+import { Box } from '@mui/system';
+import React from 'react';
+
+import { AnimatedSwitch } from 'react-router-transition';
+import { mapStyles, routeAnimation } from './animationStyle';
+import './transitions.css';
+
+
+interface TransitionProps {
+    children: React.ReactNode | React.ReactNodeArray,
+}
+
+
+export default function AnimatedRouterSwitch(props: TransitionProps) {
+    return (
+        <Box component={AnimatedSwitch}
+            {...routeAnimation}
+            mapStyles={mapStyles}
+            className="switch-wrapper"
+            runOnMount={true}
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                width: '100%',
+                alignItems: 'center',
+                height: '65%',
+            }}>
+            {props.children}
+        </Box>
+    );
+}
