23d1d59e54f8601747fec8417a323b13f82f680e
Szymon Rykała <szymon.rykala@atos.net>
2021-11-05 12:58:10 +0100

    add universal AppLink component

---
+++ src/shared/components/AppLink.tsx
@@ -0,0 +1,25 @@
+import { Link as RouterLink } from "react-router-dom";
+
+import { Link as LinkIcon } from "@mui/icons-material";
+
+import { Link, LinkProps } from "@mui/material";
+
+
+interface AppLinkProps extends LinkProps {
+    to: string,
+    withIcon?: boolean
+}
+
+export default function AppLink(props: AppLinkProps) {
+    return (
+        <Link
+            component={RouterLink}
+            underline="hover"
+            display='flex'
+            {...props}
+        >
+            {props.withIcon && <LinkIcon fontSize="small" /> + '&nbsp;'}
+            {props.children}
+        </Link >
+    );
+}
