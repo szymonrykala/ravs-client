66f9400b4860da4fdb7571171fe618622fb082fc
Szymon Rykała <szymon.rykala@atos.net>
2021-11-08 00:54:50 +0100

    update(theme): add default styling

--- src/shared/Theme/theme.ts
+++ src/shared/Theme/theme.ts
@@ -1,31 +1,33 @@
 import { createTheme } from '@mui/material/styles';
-import { red, blue, indigo } from '@mui/material/colors';
+import { indigo } from '@mui/material/colors';
+import { darkScrollbar as scrollbar } from '@mui/material';
 
 
-// const theme = createTheme({
-//   palette: {
-//     primary: {
-//       light: '#757ce8',
-//       main: '#3f50b5',
-//       dark: '#002884',
-//       contrastText: '#fff',
-//     },
-//     secondary: {
-//       light: '#ff7961',
-//       main: '#f44336',
-//       dark: '#ba000d',
-//       contrastText: '#000',
-//     },
-//   },
-// });
+const light = {
+  track: '#f5f5f5',
+  thumb: '#adadad',
+  active: '#959595'
+};
 
 const theme = createTheme({
   palette: {
     primary: indigo,
-    secondary:{
+    secondary: {
       dark: "#f5f5f5",
       main: "#304ffe",
       light: "#fff"
+    }
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: scrollbar(light),
+      },
+    },
+    MuiButton: {
+      defaultProps: {
+        variant: 'outlined'
+      }
     }
   },
   // typography:{
