1a5cdc6e72991fe11d396dbe46926c2253843d59
Szymon Rykała <szymon.rykala@atos.net>
2021-11-09 19:49:44 +0100

    add dynamic paths and minor updates

--- src/shared/path.ts
+++ src/shared/path.ts
@@ -1,6 +1,6 @@
 
 interface AppPathsInterface {
-    readonly [index: string]: string,
+    readonly [index: string]: string
     PUBLIC: string,
     LOGIN: string,
     REGISTER: string,
@@ -15,7 +15,19 @@ interface AppPathsInterface {
     ADDRESS: string,
     BUILDING: string,
     ROOM: string,
+    USERS: string
 };
+
+
+interface AppDynamicPaths {
+    readonly [indes: string]: (...params: any) => string,
+
+    toRoom: (addressId: number, buildingId: number, roomId: number) => string,
+    toBuilding: (addressId: number, buildingId: number) => string,
+    toAddress: (addressId: number) => string,
+    toUser: (userId: number) => string
+}
+
 
 const paths: { [index: string]: string } = {
     PUBLIC: '/public',
@@ -32,10 +44,18 @@ paths.REMIND_PASSWORD = paths.PUBLIC + '/passwd';
 paths.SETTINGS = paths.HOME + '/settings';
 paths.MY_PROFILE = paths.HOME + '/me';
 
+paths.USERS = paths.HOME + '/users';
 paths.INFRASTRUCTURE = paths.HOME + '/infrastructure';
 paths.ADDRESS = paths.HOME + '/addresses/:addressId';
 paths.BUILDING = paths.ADDRESS + '/buildings/:buildingId';
 paths.ROOM = paths.BUILDING + '/rooms/:roomId';
 
+
+export const dynamicPaths: AppDynamicPaths = {
+    toAddress: (addressId: number) => `/app/addresses/${addressId}`,
+    toBuilding: (addressId: number, buildingId: number) => dynamicPaths.toAddress(addressId) + `/buildings/${buildingId}`,
+    toRoom: (addressId: number, buildingId: number, roomId: number) => dynamicPaths.toBuilding(addressId, buildingId) + `/rooms/${roomId}`,
+    toUser: (userId: number) => paths.USERS + `/${userId}`
+}
 
 export default paths as AppPathsInterface;
