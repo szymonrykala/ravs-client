3a4d992519c1fb4db51a65db1646c6a9899b5831
Szymon Rykała <szymon.rykala@atos.net>
2021-11-09 19:37:16 +0100

    add Rfid generic form

---
+++ src/shared/components/RfidForm.tsx
@@ -0,0 +1,86 @@
+import { Box, Button, Stack, Typography } from "@mui/material";
+import NfcIcon from '@mui/icons-material/Nfc';
+import { styled } from "@mui/system";
+import React from "react";
+import CancelIcon from '@mui/icons-material/Cancel';
+
+
+interface RfidFormProps {
+    headline: string,
+    bodyText: string,
+    onScan: (keyText: string) => Promise<boolean>,
+    onClose: () => void,
+}
+
+const HiddenInput = styled('input')({
+    position: "absolute",
+    left: "-1000px",
+    color: "transparent"
+});
+
+
+export default function RfidForm(props: RfidFormProps) {
+    const [text, setText] = React.useState<string>('');
+    const [color, setColor] = React.useState<string>('primary');
+    const input = React.useRef<HTMLInputElement | null>(null);
+
+    const handleSubmit = async (evt: React.FormEvent) => {
+        evt.preventDefault();
+        const success = await props.onScan(text);
+        if (success) {
+            setColor('success');
+            setTimeout(handleDecline, 2000);
+        } else {
+            setColor('error');
+        }
+        setText('');
+    }
+
+    const handleDecline = () => {
+        props.onClose();
+        setText('');
+        setColor('primary');
+    }
+
+    const focusOnInput = () => {
+        input.current && console.log(input.current.focus())
+    }
+
+    return (
+        <Stack
+            onClick={focusOnInput}
+            component="form"
+            direction="column"
+            spacing={4}
+            onSubmit={handleSubmit}
+        >
+            <NfcIcon
+                sx={{ fontSize: 140, alignSelf: "center" }}
+                color={color as any}
+            />
+            <HiddenInput
+                ref={input}
+                autoFocus
+                type="text"
+                value={text}
+                onChange={(evt: any) => setText(evt.target.value)}
+            />
+            <Box component="span" textAlign='center'>
+                <Typography variant="h6">
+                    {props.headline}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {props.bodyText}
+                </Typography>
+            </Box>
+
+            <Button startIcon={<CancelIcon />}
+                onClick={handleDecline}
+                color="error"
+            >
+                Zamknij
+            </Button>
+
+        </Stack>
+    );
+}
