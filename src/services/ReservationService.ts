6241d2d61005aca7d03f1c4032f1ab60f5dce541
Szymon Rykała <szymon.rykala@atos.net>
2021-11-25 17:35:34 +0100

    refactor resolving reservation status

--- src/services/ReservationService.ts
+++ src/services/ReservationService.ts
@@ -1,3 +1,4 @@
+import { Warning } from "@mui/icons-material";
 import Reservation from "../models/Reservation";
 import { AddressViewParams } from "./AddressService";
 import { BuildingViewParams } from "./BuildingService";
@@ -29,16 +30,32 @@ export interface UpdateReservationData {
     roomId?: number
 }
 
+enum Colors {
+    success = '#edf7ed',
+    error = '#fdeded',
+    info = '#e5f6fd',
+    warning = '#fff4e5',
+    finished = '#f5f5f5'
+}
+
+interface ReservationStatus {
+    message: string,
+    color: Colors
+}
+
+
 
 class ReservationService extends Service {
 
+    private emitStatus(mess: string, color: Colors): ReservationStatus {
+        return { message: mess, color: color };
+    }
 
     public getReservations(
-        urlParams: RoomViewParams | BuildingViewParams | AddressViewParams | UserViewParams,
+        urlParams: {} | RoomViewParams | BuildingViewParams | AddressViewParams | UserViewParams,
         queryParams: ReservationsQueryParams
     ) {
         let endp = '';
-
         if ('addressId' in urlParams) {
             endp += `/addresses/${urlParams.addressId}`;
 
@@ -73,18 +90,52 @@ class ReservationService extends Service {
         return this.delete(`/reservations/${id}`);
     }
 
-    public resolveStatus(reservation: Reservation): string {
-        const now = new Date();
+    public resolveStatus(reservation: Reservation): ReservationStatus {
+
+        const nowTimestamp = Date.now();
+
+        const now = new Date(nowTimestamp);
+        const now_minus15 = new Date(nowTimestamp - 900);
+        const now_plus15 = new Date(nowTimestamp + 900);
+
         const { actualStart, plannedStart, actualEnd, plannedEnd } = reservation;
 
         const start = new Date(actualStart ? actualStart : plannedStart);
         const end = new Date(actualEnd ? actualEnd : plannedEnd);
 
-        if (now >= start && now <= end && actualStart) return "Rezerwacja trwa";
-        if (now < start) return "Jeszcze się nie rozpoczęła";
-        if (now > start && !actualStart) return "Za chwilę się rozpocznie";
-        if (now > end && actualEnd) return "Rezerwacja minęła";
-        return "Błąd rozpoznania statusu";
+
+        // nie rozpoczęła się
+        if (!actualStart) {
+            // jest już po czasie, i czeka do +15 minut na odbiór
+            if (start < now && start > now_minus15) return this.emitStatus('Za chwilę zostanie odebrana.', Colors.warning);
+
+            // za 15 minut zaczyna się rezerwacja
+            if (start > now && start < now_plus15) return this.emitStatus('Powinna zostać odebrana', Colors.info);
+
+            // nie została odbrana w czasie <planowany start + 15 minut>
+            if (start < now_plus15) return this.emitStatus('Nie odebrana.', Colors.error)
+
+            // rozpoczęła się
+        } else if (actualStart) {
+            // zakończyła się
+            if (actualEnd) return this.emitStatus('Rezerwacja zakończona.', Colors.finished);
+
+            // nie zakończyła się
+            if (!actualEnd) {
+                // kończy się w ciągu 15 minut
+                if (now < end && now_minus15 > end) return this.emitStatus('Za chwilę się kończy.', Colors.info);
+
+                // powinna się już zakończyć, ale dajemy +15 minut na zkończenie 
+                if (now > end && end > now_plus15) return this.emitStatus('Powinna się zakończyć.', Colors.warning);
+
+                // powinna się już zakończyć, ale dajemy +15 minut na zkończenie 
+                if (now > end && end < now_plus15) return this.emitStatus('Czas minął, klucz nie oddany.', Colors.error);
+
+                return this.emitStatus('Rezerwacja trwa.', Colors.success);
+            }
+        }
+
+        return this.emitStatus("Status nieznany", Colors.warning);
     }
 }
 
