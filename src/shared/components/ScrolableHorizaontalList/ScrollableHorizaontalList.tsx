0370c91471f14217c7c69d4cfbb99da44ee38ce7
Szymon Rykała <szymon.rykala@atos.net>
2021-11-30 21:23:17 +0100

    update props to use SxProps

--- src/shared/components/ScrolableHorizaontalList/ScrollableHorizaontalList.tsx
+++ src/shared/components/ScrolableHorizaontalList/ScrollableHorizaontalList.tsx
@@ -1,4 +1,4 @@
-import { List, Typography } from "@mui/material";
+import { List } from "@mui/material";
 import Box from "@mui/system/Box";
 import React from "react";
 import { AddItemButton } from ".";
@@ -7,23 +7,17 @@ import { AddItemButton } from ".";
 interface ScrollableHorizaontalListProps {
     onAddItem: () => void,
     children: React.ReactNode[] | React.ReactNode,
-    title: string
 }
 
 
 export default function ScrollableHorizaontalList(props: ScrollableHorizaontalListProps) {
     return (
         <Box>
-            <Typography variant="h6" color='text.secondary'>
-                {props.title}
-            </Typography>
-
             <List sx={{
                 display: 'flex',
                 flexDirection: 'row',
                 bgcolor: 'background.default',
                 justifyItems: 'flex-start',
-                borderRadius: ({ shape }) => shape.borderRadius,
                 overflow: 'auto',
                 padding: '15px',
                 minHeight: '160px',
