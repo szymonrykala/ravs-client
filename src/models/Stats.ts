b0aab3477c4002d8e3ddab1c26e7c59704c9676f
Szymon Rykała <szymon.rykala@atos.net>
2021-11-30 12:51:15 +0100

    fixup! add Charts utils components and service

--- src/models/Stats.ts
+++ src/models/Stats.ts
@@ -1,36 +1,70 @@
 
 interface StatsItem {
+    [index: string]: string | number
     reservationsCount: number,
-    averagePlannedTimeMinutes: number,
-    averageActualTimeMinutes: number,
+    avgPlannedTimeMinutes: number,
+    avgActualTimeMinutes: number,
     allTimeMinutes: number
 }
 
-export interface WeeklyStatsItem extends StatsItem {
-    dayOfWeek: number,
-    dayLabel: string
+
+export interface DayStatsItem extends StatsItem {
+    day: number
 }
 
-export interface MonthlyStatsItem extends StatsItem {
-    dayOfMonth: number,
-}
 
 export interface UserStatsItem extends StatsItem {
     id: number,
     email: string
 }
 
-export interface LogStat {
-    method: string
-    calls: number
+
+export interface LogStatsItem {
+    [index: string]: string | number,
+    method: string,
+    calls: number,
     generalEndpoint: string,
     avgTime: number,
     timeForEndpoint: number
 }
 
 
-export interface RoomStats {
-    dayOfWeek: [
+export interface ReservationStatsItem extends StatsItem {
+    id: number,
+    name: string,
+}
 
-    ]
+
+export interface ReservedRoomStatItem extends StatsItem {
+    roomName: string,
+    buildingName: string,
+}
+
+
+
+/*#===== C O L L E C T I O N S =====#*/
+export interface BuildingsOrRoomsChartsData {
+    allReservations: ReservationStatsItem[]
+}
+
+
+export interface BuildingOrRoomChartsData {
+    weekly: DayStatsItem[],
+    monthly: DayStatsItem[],
+    users: UserStatsItem[]
+}
+
+
+export interface LogsChartsData {
+    endpoints: LogStatsItem[]
+}
+
+export interface AllUsersChartsData {
+    users: UserStatsItem[]
+}
+
+export interface UserChartsData {
+    weekly: DayStatsItem[],
+    monthly: DayStatsItem[],
+    reservedRooms: ReservedRoomStatItem[],
 }
