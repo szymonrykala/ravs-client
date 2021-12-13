864be9a02ff8b3c5d8e5bc55cddb39be6131ac8f
Szymon Rykała <szymonrykala@gmail.com>
2021-12-14 00:08:43 +0100

    add service worker

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker.js';
 
 
 ReactDOM.render(
@@ -11,3 +12,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+serviceWorker.register();
