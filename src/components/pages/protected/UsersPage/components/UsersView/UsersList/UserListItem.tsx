ee069ae80ea904b1f13ca48dd2640887c55719b7
Szymon Rykała <szymonrykala@gmail.com>
2021-12-14 23:24:33 +0100

    refactor users list

--- src/components/pages/protected/UsersPage/components/UsersView/UsersList/UserListItem.tsx
+++ src/components/pages/protected/UsersPage/components/UsersView/UsersList/UserListItem.tsx
@@ -1,12 +1,10 @@
+import { Link } from "@mui/material";
 import Avatar from "@mui/material/Avatar";
 import ListItem from "@mui/material/ListItem";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import ListItemText from "@mui/material/ListItemText";
-import TableCell from "@mui/material/TableCell";
-import TableRow from "@mui/material/TableRow";
 import User from "../../../../../../../models/User";
 import ImageService from "../../../../../../../services/ImageService";
-import AppLink from "../../../../../../../shared/components/AppLink";
 import { dynamicPaths } from "../../../../../../../shared/path";
 import { displayDate } from "../../../../../../../shared/utils";
 
@@ -18,31 +16,26 @@ interface UserListItemProps {
 
 export default function UserListItem({ user }: UserListItemProps) {
     return (
-        <TableRow >
-            <TableCell align="left">
-                <ListItem component='div'>
-                    <ListItemAvatar
-                        title={user.email}
-                        aria-label={user.email}
-                    >
-                        <AppLink to={dynamicPaths.toUser(user.id)}>
-                            <Avatar src={ImageService.getLink(user.image)} />
-                        </AppLink>
-                    </ListItemAvatar>
-                    <ListItemText
-                        primary={<AppLink to={dynamicPaths.toUser(user.id)}>
-                            {user.name} {user.surname}
-                        </AppLink>
-                        }
-                        secondary={user.email}
-                    />
-                </ListItem>
-            </TableCell>
-            <TableCell sx={{
-                width: 'fit-content'
-            }}>
-                {displayDate(user.lastActivity)}
-            </TableCell>
-        </TableRow>
+        <ListItem button
+            component={Link}
+            href={dynamicPaths.toUser(user.id)}
+        >
+            <ListItemAvatar
+                title={user.email}
+                aria-label={user.email}
+            >
+                <Avatar src={ImageService.getLink(user.image)} />
+            </ListItemAvatar>
+            <ListItemText
+                primary={`${user.name} ${user.surname}`}
+                secondary={
+                    <>
+                        {user.email}
+                        <br />
+                        {displayDate(user.lastActivity)}
+                    </>
+                }
+            />
+        </ListItem>
     );
 }
