44ab9128121ca1672ebcfec787a13bea528a79f1
Szymon Rykała <szymonrykala@gmail.com>
2021-12-19 00:12:59 +0100

    change footer links logic

--- src/components/Footer/Links.tsx
+++ src/components/Footer/Links.tsx
@@ -1,6 +1,9 @@
-import { Grid, Link, List, ListItem } from "@mui/material";
+import { Container, Link, List, ListItem } from "@mui/material";
+import Box from "@mui/system/Box";
 import React from "react";
+import useSession from "../../auth/useSession";
 import paths from "../../shared/path";
+import useResolvedAccess from "../pages/protected/hooks/useResolvedAccess";
 
 
 interface LinkListItem {
@@ -9,49 +12,74 @@ interface LinkListItem {
 }
 
 
-const linksList: LinkListItem[][] = [
-    [
-        { name: 'Logowanie', link: paths.LOGIN },
-        { name: 'Rejestracja', link: paths.REGISTER },
-        { name: 'Aktywacja Konta', link: paths.ACTIVATE },
-
-    ],
-    [
-        { name: 'Home', link: paths.HOME },
-        { name: 'Profil', link: paths.MY_PROFILE },
-    ]
+const linksList: LinkListItem[] = [
+    { name: 'Logowanie', link: paths.LOGIN },
+    { name: 'Rejestracja', link: paths.REGISTER },
+    { name: 'Aktywacja Konta', link: paths.ACTIVATE },
 ];
 
 
 
 export default function Links() {
+    const { user } = useSession();
+    const access = useResolvedAccess();
 
-    const links = React.useMemo(() =>
-        linksList.map((list, index) => <Grid key={index} item>
-            <List>
-                {list.map(({ name, link }) => <ListItem key={name}>
+
+    const resolvedLinksList = React.useMemo(() => {
+        const list = [...linksList];
+        if (user) {
+            list.push({ name: 'Mój Profil', link: paths.MY_PROFILE }, { name: 'Home', link: paths.HOME });
+            access.accessAdmin && list.push({ name: 'Dostępy', link: paths.ACCESS });
+            access.owner && list.push({ name: 'Ustawienia', link: paths.SETTINGS });
+        }
+        return list;
+    }, [
+        user,
+        access?.accessAdmin,
+        access?.owner,
+    ]);
+
+
+    const links = React.useMemo(() => {
+
+        return resolvedLinksList.filter(item => item.link)
+            .map(({ name, link }) =>
+                <ListItem
+                    key={name}
+                    sx={{
+                        width: '50%',
+                        minWidth: '200px'
+                    }}>
                     <Link
-                        href={link}
                         sx={{ color: 'background.default' }}
+                        href={link}
                     >
                         {name}
                     </Link>
-                </ListItem>)}
-            </List>
-        </Grid>), []);
+                </ListItem>
+            );
+    }, [
+        resolvedLinksList
+    ]);
 
     return (
-        <Grid
+        <Box
             sx={{
-                backgroundColor: "primary.main",
+                backgroundColor: "primary.main"
             }}
-
-            container
-            direction="row"
-            justifyContent="space-evenly"
-            alignItems="flex-start"
         >
-            {links}
-        </Grid>
+            <Container>
+                <List sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignContent: 'center',
+                    justifyContent: 'flex-start',
+                    flexWrap: 'wrap'
+                }}>
+
+                    {links}
+                </List>
+            </Container>
+        </Box>
     );
 }
