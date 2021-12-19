61de8ee6cec58262bcf1a93acfae3e8eab18c758
Szymon Rykała <szymonrykala@gmail.com>
2021-12-19 16:04:26 +0100

    enable pwa

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker.js';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 ReactDOM.render(
@@ -13,4 +13,4 @@ ReactDOM.render(
 );
 
 
-serviceWorker.register();
+serviceWorkerRegistration.register();
