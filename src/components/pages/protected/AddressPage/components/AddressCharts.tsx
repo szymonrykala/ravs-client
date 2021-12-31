24d9e791efd36fd15810d714b9e7fe7f19b30051
Szymon Rykała <szymonrykala@gmail.com>
2021-12-31 13:33:47 +0100

    implement generating stats pdfs

--- src/components/pages/protected/AddressPage/components/AddressCharts.tsx
+++ src/components/pages/protected/AddressPage/components/AddressCharts.tsx
@@ -1,5 +1,11 @@
 import { BuildingsOrRoomsChartsData } from "../../../../../models/Stats";
 import { Chart, ChartSection, ChartsTab, useCharts } from "../../components/Charts";
+import { useAddress } from "../AddressContext";
+import { usePDF } from "@react-pdf/renderer";
+import { Button } from "@mui/material";
+import PDFPage from "../../components/PDF/PDFPage";
+import Table from "../../components/PDF/Table";
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
 
 
 
@@ -13,37 +19,77 @@ export default function AddressCharts() {
 
 function ActualLoader() {
     const { chartsData } = useCharts<BuildingsOrRoomsChartsData>();
+    const { address } = useAddress();
+
+    const [PDF] = usePDF({
+        document: <PDFPage
+            subject={`Statystyki adresu ${address.town}, ${address.street} ${address.number}`}
+            sections={
+                [
+                    <Table
+                        title="Statystyki dla budynków pod adresem"
+                        rows={[
+                            [
+                                'nazwa budynku',
+                                'ilość rezerwacji',
+                                'czas rezerwacji',
+                                'średni czas rezerwacji',
+                                'średni planowany czas rezerwacji'
+                            ]
+                            , ...chartsData.allReservations.map(r => [
+                                r.name,
+                                r.reservationsCount,
+                                r.allTimeMinutes,
+                                r.avgActualTimeMinutes,
+                                r.avgPlannedTimeMinutes
+                            ])
+                        ]}
+                    />
+                ]} />
+    });
 
     return (
-        <ChartSection defaultOpen title='Statystyki budynków'>
-            <Chart
-                fullWidth
-                title='Ilość rezerwacji'
-                data={chartsData.allReservations}
-                xKey='name'
-                y={[
-                    { label: 'Ilość rezerwacji', key: 'reservationsCount' }
-                ]}
-            />
-            <Chart
-                fullWidth
-                title="Czas wszystkich reserwacji"
-                data={chartsData.allReservations}
-                xKey='name'
-                y={[
-                    { label: 'Czas całkowity', key: 'allTimeMinutes' },
-                ]}
-            />
-            <Chart
-                fullWidth
-                title="Średnie czasy rezerwacji"
-                data={chartsData.allReservations}
-                xKey='name'
-                y={[
-                    { label: 'Średni planowany czas', key: 'avgPlannedTimeMinutes' },
-                    { label: 'Średni faktyczny czas', key: 'avgActualTimeMinutes' },
-                ]}
-            />
-        </ChartSection>
+        <>
+            {
+                (PDF.loading || !PDF.url) ?
+                    <p>loading pdf ...</p>
+                    :
+                    <Button startIcon={<PictureAsPdfIcon />}>
+                        <a href={PDF.url} download={'stats.pdf'} title='pobierz tabele pdf'>
+                            Pobierz raport pdf
+                        </a>
+                    </Button>
+            }
+            <ChartSection defaultOpen title='Statystyki budynków'>
+                <Chart
+                    fullWidth
+                    title='Ilość rezerwacji'
+                    data={chartsData.allReservations}
+                    xKey='name'
+                    y={[
+                        { label: 'Ilość rezerwacji', key: 'reservationsCount' }
+                    ]}
+                />
+                <Chart
+                    fullWidth
+                    title="Czas wszystkich reserwacji"
+                    data={chartsData.allReservations}
+                    xKey='name'
+                    y={[
+                        { label: 'Czas całkowity', key: 'allTimeMinutes' },
+                    ]}
+                />
+                <Chart
+                    fullWidth
+                    title="Średnie czasy rezerwacji"
+                    data={chartsData.allReservations}
+                    xKey='name'
+                    y={[
+                        { label: 'Średni planowany czas', key: 'avgPlannedTimeMinutes' },
+                        { label: 'Średni faktyczny czas', key: 'avgActualTimeMinutes' },
+                    ]}
+                />
+            </ChartSection>
+        </>
     );
 };
--- src/components/pages/protected/UserPage/components/UserCharts.tsx
+++ src/components/pages/protected/UserPage/components/UserCharts.tsx
@@ -2,6 +2,14 @@ import { UserChartsData } from "../../../../../models/Stats";
 import { Chart, ChartSection, ChartsTab, useCharts } from "../../components/Charts";
 import MonthlyChartsSection from "../../components/MonthlyChartsSection";
 import WeeklyChartsSection from "../../components/WeeklyChartsSection";
+import { Button } from "@mui/material";
+import { usePDF } from "@react-pdf/renderer";
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
+import PDFPage from "../../components/PDF/PDFPage";
+import Table from "../../components/PDF/Table";
+import { weekDays } from "../../components/Charts/Chart";
+import { useUser } from "../UserContext";
+
 
 
 export default function UserCharts() {
@@ -15,9 +23,86 @@ export default function UserCharts() {
 
 function ActualLoader() {
     const { chartsData } = useCharts<UserChartsData>();
+    const { user } = useUser();
+
+    const [PDF] = usePDF({
+        document: <PDFPage
+            subject={`Statystyki użytkownika ${user.name} ${user.surname}, ${user.email}`}
+            sections={
+                [
+                    <Table
+                        title="Rezerwowane sale"
+                        rows={[
+                            [
+                                'nazwa sali',
+                                'ilość rezerwacji',
+                                'czas rezerwacji',
+                                'średni czas rezerwacji',
+                                'średni planowany czas rezerwacji'
+                            ]
+                            , ...chartsData.reservedRooms.map(r => [
+                                `${r.buildingName} - ${r.roomName}`,
+                                r.reservationsCount,
+                                r.allTimeMinutes,
+                                r.avgActualTimeMinutes,
+                                r.avgPlannedTimeMinutes
+                            ])
+                        ]}
+                    />,
+                    <Table
+                        title="Statystyki tygodniowe"
+                        rows={[
+                            [
+                                'dzień tygodnia',
+                                'ilość rezerwacji',
+                                'czas rezerwacji',
+                                'średni czas rezerwacji',
+                                'średni planowany czas rezerwacji'
+                            ]
+                            , ...chartsData.weekly.map(r => [
+                                weekDays[r.day],
+                                r.reservationsCount,
+                                r.allTimeMinutes,
+                                r.avgActualTimeMinutes,
+                                r.avgPlannedTimeMinutes
+                            ])
+                        ]}
+                    />,
+                    <Table
+                        title="Statystyki miesięczne"
+                        rows={[
+                            [
+                                'dzień miesiąca',
+                                'ilość rezerwacji',
+                                'czas rezerwacji',
+                                'średni czas rezerwacji',
+                                'średni planowany czas rezerwacji'
+                            ]
+                            , ...chartsData.monthly.map(r => [
+                                r.day,
+                                r.reservationsCount,
+                                r.allTimeMinutes,
+                                r.avgActualTimeMinutes,
+                                r.avgPlannedTimeMinutes
+                            ])
+                        ]}
+                    />
+                ]} />
+    });
 
     return (
         <>
+            {
+                (PDF.loading || !PDF.url) ?
+                    <p>loading pdf ...</p>
+                    :
+                    <Button startIcon={<PictureAsPdfIcon />}>
+                        <a href={PDF.url} download={'stats.pdf'} title='pobierz tabele pdf'>
+                            Pobierz raport pdf
+                        </a>
+                    </Button>
+            }
+
             <ChartSection defaultOpen title='Rezerwowane Sale'>
                 <Chart
                     fullWidth
